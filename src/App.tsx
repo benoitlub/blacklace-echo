0c3c127f45f4c07f82dad09278344ce028fc1432
benoitlub <benoitlubert@gmail.com>
2026-05-26 11:20:27 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/blacklace-echo">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/map" element={<Map />} />
