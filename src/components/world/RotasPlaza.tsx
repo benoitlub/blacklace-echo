7377ff861a0f0b579267885eb8adcb2cabe8fcb8
benoitlub <benoitlubert@gmail.com>
2026-06-07 12:03:04 +0200

    Restore minimal Rotas plaza component

--- src/components/world/RotasPlaza.tsx
+++ src/components/world/RotasPlaza.tsx
@@ -1,13 +1,5 @@
-import React from "react";
-import "@/styles/rotas.css";
+type Props={entering:boolean;onBack:()=>void};
 
-type Props = { entering: boolean; onBack: () => void };
-
-export default function RotasPlaza(props: Props) {
-  const cls = props.entering ? "rotas-plaza is-entering" : "rotas-plaza is-ready";
-  return React.createElement("section", { className: cls },
-    React.createElement("div", { className: "rotas-sky" }),
-    React.createElement("div", { className: "rotas-sea" }),
-    React.createElement("div", { className: "rotas-world" },
-      React.createElement("div", { className: "rotas-building tea-house" },
-        React.createElement("span", { className: "rot
+export default function RotasPlaza({entering,onBack}:Props){
+  return <section className={entering?"rotas-plaza is-entering":"rotas-plaza is-ready"}><button className="rotas-back" onClick={onBack}>Retour carte</button></section>;
+}
