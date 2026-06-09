8fdb34daf970c49766a197611eb8939319806d8a
benoitlub <benoitlubert@gmail.com>
2026-06-09 19:11:42 +0200

    Load Rotas real background fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import "./styles/blacklace.css";
 import "./styles/blacklace-video-fix.css";
 import "./styles/feuch-modal-fix.css";
+import "./styles/rotas-real-fix.css";
 import Index from "./pages/Index.tsx";
 import Map from "./pages/Map.tsx";
 import Aloisia from "./pages/Aloisia.tsx";
