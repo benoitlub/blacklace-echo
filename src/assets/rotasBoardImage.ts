22442f12e78ba6cdb9d61be2c13490ad07bbd463
benoitlub <benoitlubert@gmail.com>
2026-06-09 23:50:27 +0200

    Use uploaded Rotas floating board asset

--- src/assets/rotasBoardImage.ts
+++ src/assets/rotasBoardImage.ts
@@ -1,3 +1,1 @@
-import { ROTAS_TRUE_IMAGE } from "./rotasTrueImage";
-
-export const ROTAS_BOARD_IMAGE = ROTAS_TRUE_IMAGE;
+export const ROTAS_BOARD_IMAGE = `${import.meta.env.BASE_URL}assets/img/file_00000000176c71f48f0d649efd5a3b26.png`;
